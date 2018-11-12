99aea5a42941cbd2bd6a4edf6726ecc5658d8b21
Srinath Kailasa <srinathkailasa@gmail.com>
2018-11-12 17:27:34 +0000

    cpp 'hello world' app

---
+++ cpp/hello_world_app/add.h
@@ -0,0 +1,12 @@
+// The header file, where the forwward declarations are placed
+// This let's you save a lot of copying and pasting
+// Begin with a header guard, to stop them from being imported
+// More than once
+
+#ifndef ADD_H //convention to use the name of the header file, can be any unique name
+#define ADD_H
+
+// content of header file, where declarations go
+int add(int x, int y); // function prototype for add, i.e. the forward declaration
+#endif // end of header guard
+
