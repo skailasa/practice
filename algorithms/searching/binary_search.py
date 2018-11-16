f46bf9496887f572cc405517887a14419d7dbc68
Sri <srinathkailasa@gmail.com>
2018-11-16 14:42:09 +0000

    Add binary search, as well as rotated array search implementation

--- algorithms/searching/binary_search.py
+++ algorithms/searching/binary_search.py
@@ -2,33 +2,32 @@
 Implementation of Binary Search
 """
 
-class Binary:    
+
+class Binary:
     def __init__(self, A):
         self.A = A
 
     def iterative_search(self, x):
         start = 0
         end = len(self.A)
-        
-        
-        while start < end: 
-            mid = int((start+end)/2)
+
+        while start < end:
+            mid = int((start + end) / 2)
             if self.A[mid] < x:
-                start = mid+1
-            elif self.A[mid] < x:
-                end = mid -1
-            
+                start = mid + 1
+            elif self.A[mid] > x:
+                end = mid
             else:
                 return mid
-    
-    def recursive_search(self, x, start, end): 
-        mid = int((start+end)/2)
-        
+
+    def recursive_search(self, x, start, end):
+        mid = int((start + end) / 2)
+
         if self.A[mid] < x:
-            return self.recursive_search(x, mid+1, end)
+            return self.recursive_search(x, mid + 1, end)
 
         elif self.A[mid] > x:
-            return self.recursive_search(x, start, mid-1)
+            return self.recursive_search(x, start, mid - 1)
 
         else:
             return mid
@@ -36,8 +35,8 @@ class Binary:
 
 if __name__ == "__main__":
     x = 10
-    A = [1,2,10,100,1312]
-    
+    A = [1, 2, 10, 100, 1312]
+
     print(Binary(A).iterative_search(x))
 
     print(Binary(A).recursive_search(x, 0, 4))
---
+++ ctc/ch10-sorting-and-searching/3-search-in-rotated-array.py
@@ -0,0 +1,51 @@
+"""
+Given a sorted array of n integers that has been rotated an unknown
+number of times, write code to find an element in the array. You may
+assume that the array was originally sorted in increasing order.
+
+e.g. find 5 in [15, 16, 19, 20, 25, 1, 3, 5, 6, 7, 10]
+
+outputs 7
+-
+Strategy:
+    - we know the original array was sorted in increasing order,
+        - so if there is a negative diff between consecutive elements this
+        was where the array originally started.
+"""
+
+
+def binary_search(x, A):
+
+    start = 0
+    end = len(A)
+
+    while start < end:
+        mid = int((end+start)/2)
+        if x < A[mid]:
+            end = mid
+        elif x > A[mid]:
+            start = mid+1
+        else:
+            return mid
+
+
+def find_element(x, A_rotated):
+
+    pivot = 0
+
+    for i in range(len(A_rotated)-1):
+        if A_rotated[i+1] - A_rotated[i] < 0:
+            pivot = i
+            break
+
+    if A[0] <= x < A[pivot]:
+        return binary_search(x, A_rotated[:pivot])
+
+    return binary_search(x, A_rotated[pivot:]) + pivot
+
+
+if __name__ == "__main__":
+    A = [15, 16, 19, 20, 25, 1, 3, 5, 6, 7, 10]
+    print(find_element(5, A))
+
+
