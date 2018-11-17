55de63eb434e3ded04096395c3918f6c10168d38
Sri <srinathkailasa@gmail.com>
2018-11-17 14:05:11 +0000

    Lint dfs implementation

--- algorithms/graph/depth_first_search.py
+++ algorithms/graph/depth_first_search.py
@@ -7,49 +7,50 @@ could get stuck in an inf. loop
 """
 from collections import defaultdict
 
+
 class Graph:
-	"""Represent graph using adjacency list"""
+    """Represent graph using adjacency list"""
 
-	def __init__(self):
-		self._graph = defaultdict(list)
+    def __init__(self):
+        self._graph = defaultdict(list)
 
-	def __iter__(self):
-		return iter((k,v) for k, v in self._graph.items())
+    def __iter__(self):
+        return iter((k, v) for k, v in self._graph.items())
 
-	def __getitem__(self, item):
-		return self._graph[item]
+    def __getitem__(self, item):
+        return self._graph[item]
 
-	def __setitem__(self, u, v):
-		self._graph[u].append(v)
+    def __setitem__(self, u, v):
+        self._graph[u].append(v)
 
-	def __repr__(self):
-		return str(self._graph)
+    def __repr__(self):
+        return str(self._graph)
 
 
 class DFS:
-	"""Operates on Graph objects"""
+    """Operates on Graph objects"""
 
-	def __init__(self, graph):
-		self._graph = graph
-		self._visited = {k: False for k, v in graph}
+    def __init__(self, graph):
+        self._graph = graph
+        self._visited = {k: False for k, v in graph}
 
-	def visit(self, v):
-		"""Mark visited Nodes"""
-		self._visited[v] = True
+    def visit(self, v):
+        """Mark visited Nodes"""
+        self._visited[v] = True
 
-	def search(self, root):
+    def search(self, root):
 
-		# mark root node as visited
-		self.visit(root)
-		print(root)
+        #  mark root node as visited
+        self.visit(root)
+        print(root)
 
-		for node in self._graph[root]:
-			if not self._visited[node]:
-				self.search(node)
+        for node in self._graph[root]:
+            if not self._visited[node]:
+                self.search(node)
 
 
 def main():
-	"""
+    """
 	Test out DFS implementation on graph:
 
 	0 -> 1 -> 2 ->3
@@ -57,17 +58,18 @@ def main():
 	|
 	2
 	"""
-	g = Graph()
-	g[0]=1 
-	g[0]=2
-	g[1]=2 
-	g[2]=0 
-	g[2]=3 
-	g[3]=3
+    g = Graph()
+    g[0] = 1
+    g[0] = 2
+    g[1] = 2
+    g[2] = 0
+    g[2] = 3
+    g[3] = 3
 
-	print(g) 
+    print(g)
 
-	DFS(g).search(0)
+    DFS(g).search(0)
+
 
 if __name__ == "__main__":
-	main()
+    main()
