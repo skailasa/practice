fdf3009a39529a005721f254767a83b03d5a8c22
Sri <srinathkailasa@gmail.com>
2018-11-26 17:30:38 +0000

    Fix formatting on bit manipulation tricks

--- general-concepts/bit_manipulation.py
+++ general-concepts/bit_manipulation.py
@@ -6,11 +6,11 @@ the implementation of
 
 def get_bit(num, i):
     """
-	Shifts 1 over by i bits, by performing an & with num we
-		clear all bits other than the bit at i. Finally compare
-		that to 0, if it is zero that's what we have, otherwise
-		we have a 1.
-	"""
+    Shifts 1 over by i bits, by performing an & with num we
+        clear all bits other than the bit at i. Finally compare
+        that to 0, if it is zero that's what we have, otherwise
+        we have 1.
+    """
     if ((1 << i) & num == 0):
         return 0
     return 1
@@ -18,10 +18,10 @@ def get_bit(num, i):
 
 def set_bit(num, i):
     """
-	Shifts 1 over by i bits, performs an | with num, only the value
-		at i will change to a 1 if it is 0, every other value will
-		take | with 0 keeping it the same. 
-	"""
+    Shifts 1 over by i bits, performs an | with num, only the value
+        at i will change to a 1 if it is 0, every other value will
+        take | with 0 keeping it the same.
+    """
     return (1 << i) | num
 
 
@@ -29,7 +29,7 @@ def clear_bit(num, i):
     """
     Need to create a number like 1111101111 where the number in the
         same position of 0 will be & with and therefore erased
-	"""
+    """
     mask = ~(1 << i)
     return num & mask
 
