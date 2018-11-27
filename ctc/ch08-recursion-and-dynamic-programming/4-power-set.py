55d93f6903f6cbe996c9bc849b02e248f70d8bdc
Sri <srinathkailasa@gmail.com>
2018-11-27 11:18:58 +0000

    Add power set implementation

---
+++ ctc/ch08-recursion-and-dynamic-programming/4-power-set.py
@@ -0,0 +1,30 @@
+"""
+Write a method to return all subsets of a set.
+
+Strategy:
+    - Base case, when there is just one item, return an empty set
+    - Work through, noticing that
+        n1 = n0 + [l.append(additional_element) for l in n0]
+        where 'additional_element' is the element in n1 but not in n0.
+"""
+
+
+ALL_SUBSETS = []
+
+
+def subsets(s):
+    if not s:
+        # empty set
+        return [[]]
+
+    else:
+        temp = subsets(s[1:])
+        res = [l+[s[0]] for l in temp] + temp
+
+    return res
+
+
+if __name__ == "__main__":
+    s = [1, 2, 3]
+    print(subsets(s))
+
