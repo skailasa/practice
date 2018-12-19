ed9bd6d82547bb37bd301f90581b065d0c2a933c
Sri <srinathkailasa@gmail.com>
2018-12-19 15:42:10 +0530

    Add missing number implementation with lower complexity

--- ctc/hard/4-missing-number.py
+++ ctc/hard/4-missing-number.py
@@ -13,8 +13,11 @@ Can you do this in O(N) time?
 """
 
 
-def find_missing_integer(l):
-    n = len(l)
+def missing_integer_simple(l):
+    """
+    Compute in a simple way, but O(NLog(N)) complexity
+    """
+    n = len(l)-1
     expected = n*(n+1)/2
     found = 0
 
@@ -25,10 +28,75 @@ def find_missing_integer(l):
     print(expected-found)
 
 
+def count(l, shift=0):
+    c = {0: 0, 1: 0}
+
+    for n in l:
+        if bin(n >> shift)[-1] == '0':
+            c[0] += 1
+        elif bin(n >> shift)[-1] == '1':
+            c[1] += 1
+
+    return c
+
+
+def missing_integer(l, shift):
+
+    c = count(l[0], shift)
+    missing_int = ''
+    even = False
+    print(c)
+    if l[1] % 2 == 0:
+        even = True
+
+    if even:
+        # If the list is of even length expect equal number of 0s and 1s
+        if c[0] == c[1] + 1:
+            # a one is missing, meaning that the missing integer is odd
+            missing_int += '1'
+        elif c[1] == c[0] + 1:
+            # a zero is missing, meaning that the missing integer is even
+            missing_int += '0'
+
+    else:
+        # If the list is of odd length, expect #0s = #1s + 1
+        if c[0] == c[1]:
+            # a zero is missing
+            missing_int += '0'
+
+        elif c[0] == c[1] + 2:
+            # a one is missing
+            missing_int += '1'
+
+    return missing_int
+
+
+def filter_evens(l, shift):
+    res = []
+    for n in l:
+        if bin(n >> shift)[-1] == '0':
+            res.append(n)
+
+    return res
+
+
+def filter_odds(l, shift):
+    res = []
+    for n in l:
+        if bin(n >> shift)[-1] == '1':
+            res.append(n)
+    return res
+
+
 if __name__ == "__main__":
 
-    l = [1, 2, 3, 5]  # separated from missing number by 2^2
-
-    find_missing_integer(l)
-
-
+    # a six is missing, 110 in binary
+    l = ([0, 1, 2, 3, 4, 5], 7)
+    print(missing_integer(l, 0))
+    # should output 0, therefore filter out odds
+    print(filter_evens(l[0], 0))
+    # results in [0, 2], put this back into algorithm
+    print(missing_integer(([0, 4, 2], 4), 1))
+    # results in 1, filter out evens [2]
+    print(filter_odds([0, 4, 2], 1))
+    print(missing_integer(([2], 2), 2))
