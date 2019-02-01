e451469fc6590a4fa7ea70c0d04c049b6902bacf
Sri <srinathkailasa@gmail.com>
2019-02-01 14:12:59 +0000

    Refine book store type

--- haskell/real-world-haskell/ch03/BookStore.hs
+++ haskell/real-world-haskell/ch03/BookStore.hs
@@ -1,8 +1,31 @@
-data BookInfo = Book Int String [String]
+type BookID = Int
+type BookTitle = String
+type BookAuthors = [String]
+
+data BookInfo = Book BookID BookTitle BookAuthors
                 deriving (Show)
 
 data MagazineInfo = Magazine Int String [String]
                     deriving (Show)
 
+-- Type and Value constructors can/normally do, have the same
+-- name in Haskell
+-- Can alias common types to make more descriptive.
+type CustomerID = Int
+type ReviewBody = String
+
+data BookReview = BookReview BookInfo CustomerID ReviewBody
+                  deriving (Show)
+
+-- Can also use synonyms for more verbose types
+type BookRecord = (BookInfo, BookReview)
+
+-- Can pattern match on algebraic datatypes
+bookID (Book id _ _) = id
+bookTitle (Book _ title _) = title
+bookAuthors (Book _ _ authors) = authors
+
 myInfo = Book 12345 "Algebra of Programming"
          ["Richard Bird", "Oege de Moor"]
+
+myReview = BookReview myInfo 1 "It was OK"
