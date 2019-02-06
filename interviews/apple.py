dc71d4fc439a0d0db8575a95e43fc6146dde8c0d
Sri <srinathkailasa@gmail.com>
2019-02-06 22:03:27 +0000

    Begin tests

--- interviews/apple.py
+++ interviews/apple.py
@@ -27,52 +27,83 @@ Output: 3
 Strategy:
 - Assume you know all possible colours a car can take in a given dataset,
 and can't query or colours other than these
+
+TODO:
+- Tests
+- Make operations all in-place and explicitly so
+- Complexity analysis
 """
 
 
-DATA_STREAM = 'a o b a d b x r end'
+def sub_array_generator(colour, colour_data):
+    """
+    Generate the left and right sub arrays for a given colour of the
+    algorithm, yielding sub arrays from subsequent occurrences of the
+    same colour.
+
+    :param str colour: The colour being searched for.
+    :param list[str] colour_data: The data set containing the colour of
+    each car.
+    :returns tuple[list[str], list[str]]: The left and right sub arrays
+    from a given colour. Yield left sub array in reverse order.
+    """
+    for idx, c in enumerate(colour_data):
+        if c == colour:
+            yield colour_data[idx+1:], list(reversed(colour_data[:idx]))
 
 
-def start_point(c, data):
-    """Generate the starting point of the algorithm"""
-    for idx, value in enumerate(data):
-        if value == c:
-            yield idx+1, data[idx+1:]
-
-
-def min_distance(c1, c2, data):
-    """Find the minimum distance"""
-    if c1 == c2:
+def min_distance(colour_1, colour_2, colour_data):
+    """
+    Find the minimum distance between two cars of specified colours in
+    some data.
+    :param str colour_1: The colour of the first car.
+    :param str colour_2: The colour of the second car.
+    :param list[str] colour_data: The data set containing the colours of
+    all the cars
+    :return int: The minimum distance between cars of colour_1 and
+    colour_2.
+    """
+    if colour_1 == colour_2:
         return 0
 
     distances = []
-    for idx, dat in start_point(c1, data):
-        forward = dat
-        backward = list(reversed(data[:idx-1]))
 
-        directions = [forward, backward]
+    for r_sub_array, l_sub_array in sub_array_generator(colour_1, colour_data):
 
-        for direction in directions:
+        sub_arrays = [r_sub_array, l_sub_array]
+
+        for sub_array in sub_arrays:
             distance = 0
-            for idx, c in enumerate(direction):
-                if idx == len(direction) - 1:  # end of list without finding
-                    distance = 1000000  # some very large distance
+            for idx, colour in enumerate(sub_array):
+                # Reached end of sub array without finding colour_2
+                if idx == len(sub_array) - 1:
+                    # Set to some very large distance
+                    distance = 1000000
                     distances.append(distance)
-                else:
-                    distance += 1
-                    if c == c2:
-                        if distance == 1:  # dont loop any further if we found min distance
-                            return distance
-                        distances.append(distance)
-                        break
+
+                distance += 1
+                if colour == colour_2:
+                    # Don't loop any further if we found min distance
+                    if distance == 1:
+                        return distance
+                    distances.append(distance)
+                    break
 
     return min(distances)
 
 
 if __name__ == "__main__":
-    data = DATA_STREAM.split((' '))
+    data_stream = 'a o b a d b x r end'
+    data = data_stream.split((' '))
     c1 = 'r'
-    c2 = 'b'
+    c2 = 'x'
+
     print("Input data \n", data)
-    print("min distance between '{}' and '{}' is {}".format(c1, c2, min_distance(c1, c2, data)))
+    print(
+        "Minimum distance between '{}' and '{}' is {}".format(
+            c1,
+            c2,
+            min_distance(c1, c2, data)
+        )
+    )
 
---
+++ interviews/apple_test.py
@@ -0,0 +1,12 @@
+"""Unit tests for apple code test"""
+import pytest
+
+from .apple import  min_distance
+
+
+def test_sub_array_generator():
+    pass
+
+
+def test_min_distance():
+    pass
