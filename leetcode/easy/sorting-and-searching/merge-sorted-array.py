f96c36b6c0a754267640e6ff92270e2bfae2fee9
Sri <srinathkailasa@gmail.com>
2019-03-30 14:30:26 +0000

    Add merge two sorted lists implementation

---
+++ leetcode/easy/sorting-and-searching/merge-sorted-array.py
@@ -0,0 +1,44 @@
+"""
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1
+as one sorted array.
+
+Note:
+The number of elements initialized in nums1 and nums2 are m and n
+respectively.
+You may assume that nums1 has enough space (size that is greater or
+equal to m + n) to hold additional elements from nums2.
+
+Strategy:
+Iterate through both lists, compare values until
+"""
+from typing import List
+
+
+def merge(nums1: List[int], nums2: List[int])->None:
+    """Merges nums2 into nums1 in-place"""
+
+    merged = False
+    i = 0
+    j = 0
+
+    while not merged:
+
+        if nums1[i] <= nums2[j]:
+            i += 1
+
+        elif nums1[i] > nums2[j]:
+            nums1.insert(i, nums2[j])
+            j += 1
+
+        if j == len(nums2):
+            merged = True
+
+
+if __name__ == "__main__":
+    sentinel = 999999
+    l1 = [1, 2, 3, 4, sentinel]
+    l2 = [4, 5, 6, 7]
+
+    merge(l1, l2)
+
+    print(l1)
