dbdb3ce045a7e121eec20a50ea5467e602877fc5
Sri <srinathkailasa@gmail.com>
2019-04-21 17:05:03 +0100

    Add operator overloading example

---
+++ general-concepts/operatorOverloading.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+
+class Complex
+{
+public:
+    int a,b;
+    void input(string s)
+    {
+        int v1=0;
+        int i=0;
+        while(s[i]!='+')
+        {
+            v1=v1*10+s[i]-'0';
+            i++;
+        }
+        while(s[i]==' ' || s[i]=='+'||s[i]=='i')
+        {
+            i++;
+        }
+        int v2=0;
+        while(i<s.length())
+        {
+            v2=v2*10+s[i]-'0';
+            i++;
+        }
+        a=v1;
+        b=v2;
+    }
+};
+
+Complex operator +(const Complex& a, const Complex& b){
+    Complex res;
+    res.a = (a.a + b.a);
+    res.b = (a.b + b.b);
+    return res;
+}
+
+ostream& operator<<(ostream& os, const Complex& c) {
+    return os << c.a << (c.b > 0 ? '+' : '-') << 'i' << c.b;
+}
